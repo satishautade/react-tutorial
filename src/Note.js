2025ca97f8d10e3b9788fc4a3fbedc09b09161b6
Satish Autade <satishautade@gmail.com>
2018-08-28 22:23:30 +1000

    Learning React-4.2 Adding State to the Note Component cleanup extra application files Add Note component with renderDisplay and renderForm behaviour

--- src/Note.js
+++ src/Note.js
@@ -1,29 +1,62 @@
 import React, { Component } from 'react'
 import { FaEdit } from 'react-icons/fa'
 import { FaTrash } from 'react-icons/fa'
+import { FaSave } from 'react-icons/fa'
+
 class Note extends Component {
 
 	constructor(props){
 		super(props)
+		this.state = {
+			editing: false
+		}
 		this.edit= this.edit.bind(this)
 		this.delete= this.delete.bind(this)
+		this.save= this.save.bind(this)
+		this.renderForm = this.renderForm.bind(this)
+		this.renderDisplay = this.renderDisplay.bind(this)
+
 	}
+
 	edit(){
-		alert('editing note')
+		this.setState({
+			editing: true
+		})
 	}
+
 	delete(){
 		alert('deleting note')
 	}
-	render(){
+
+	save(){
+		alert('saved!')
+	}
+
+	renderForm(){
+			return (
+				<div className="form-container">
+					<form>
+						<textarea />
+						<button id="save" onClick={ this.save }><FaSave /></button>
+					</form>
+				</div>
+			)
+	}
+
+	renderDisplay(){
 		return(
 			<div className="note">
-			<p>Edit Me</p>
-			<span>
-			<button id="edit" onClick={ this.edit } ><FaEdit /></button>
-			<button id="delete" onClick={ this.delete }><FaTrash /></button>
-			</span>
+				<p>Edit Me</p>
+				<span>
+					<button id="edit" onClick={ this.edit } ><FaEdit /></button>
+					<button id="delete" onClick={ this.delete } ><FaTrash /></button>
+				</span>
 			</div>
 		)
+	}
+
+	render() {
+		 return this.state.editing ? this.renderForm() : this.renderDisplay()
 	}
 
 }
